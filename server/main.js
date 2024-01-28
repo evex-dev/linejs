c4d8d0638ea0f9705ba953138b6a92eab60c7fa1
test0987654321234567890 <141794311+test0987654321234567890@users.noreply.github.com>
2024-01-28 14:55:38 +0900

    Add files via upload

--- server/main.js
+++ server/main.js
@@ -5,16 +5,16 @@ import read from "./read_deno.js"
 
 export async function handler(request) {
   const url = new URL(request.url)
-  
-    switch (url.pathname) {
-      case "/j2t":
-        return await j2t(request);
-      case "/t2j":
-        return await t2j(request);
-      default:
-        break;
-    }
-  
+
+  switch (url.pathname) {
+    case "/j2t":
+      return await j2t(request);
+    case "/t2j":
+      return await t2j(request);
+    default:
+      break;
+  }
+
   switch (url.pathname) {
     case "/api":
       return new Response(await Deno.readTextFile("./site/api.html"), { headers: { "content-type": "text/html" } });
@@ -26,12 +26,16 @@ export async function handler(request) {
 
 async function j2t(request) {
   if (request.headers.get("upgrade") === "websocket") {
-      const { socket, response } = Deno.upgradeWebSocket(request);
-      socket.onmessage = (event) => {
+    const { socket, response } = Deno.upgradeWebSocket(request);
+    socket.onmessage = (event) => {
+      try {
         const Trequest = write(JSON.parse(event.data))
         socket.send(Trequest);
-      };
-      return response;
+      } catch (e) {
+        socket.send(e)
+      }
+    };
+    return response;
   }
   let json = await request.json()
   const Trequest = write(json)
@@ -39,13 +43,17 @@ async function j2t(request) {
 }
 async function t2j(request) {
   if (request.headers.get("upgrade") === "websocket") {
-      const { socket, response } = Deno.upgradeWebSocket(request);
-      socket.onmessage = (event) => {
+    const { socket, response } = Deno.upgradeWebSocket(request);
+    socket.onmessage = (event) => {
+      try {
         let req = new Uint8Array(event.data)
         const Tresponse = read(req)
         socket.send(JSON.stringify(Tresponse));
-      };
-      return response;
+      } catch (e) {
+        socket.send(e);
+      }
+    };
+    return response;
   }
   let req = await request.arrayBuffer()
   req = new Uint8Array(req)
