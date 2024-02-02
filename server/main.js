1faf443c1aaa679b2cacd116c2d0f8f54ffe7f8a
test0987654321234567890 <141794311+test0987654321234567890@users.noreply.github.com>
2024-02-02 13:57:03 +0900

    Add files via upload

--- server/main.js
+++ server/main.js
@@ -11,10 +11,27 @@ export async function handler(request) {
       break;
   }
   try {
-    return new Response(await Deno.readTextFile("./site"+url.pathname), { headers: { "content-type": "text/html" } });
+    let mimeType = "text/html"
+    switch (url.pathname.split(".")[url.pathname.split(".").length - 1]) {
+      case "html":
+        mimeType = "text/html"
+        break;
+      case "js":
+        mimeType = "application/javascript"
+        break;
+      case "svg":
+        mimeType = "image/svg+xml"
+        break;
+      case "css":
+        mimeType = "text/css"
+        break;
+      default:
+        break;
+    }
+    return new Response(await Deno.readTextFile("./site" + url.pathname), { headers: { "content-type": mimeType } });
   } catch (error) {
   }
-    return new Response(await Deno.readTextFile("./site/index.html"), { headers: { "content-type": "text/html" ,"server":"error"} })
+  return new Response(await Deno.readTextFile("./site/index.html"), { headers: { "content-type": "text/html", "deno-status": "error" } })
 }
 
 
