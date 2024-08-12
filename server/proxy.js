7457579b0398bb061561abc655f69c5c162b09e8
test0987654321234567890 <test0987654321234567890@line-selfbot.deno.dev>
2024-08-12 18:29:00 +0900

    ver 0.2.1+

--- server/proxy.js
+++ server/proxy.js
@@ -1,5 +1,4 @@
 export default async function proxy(request) {
-<<<<<<< HEAD
     const url = new URL(request.url)
     const domain = atob(url.pathname.split("/")[2])
     const path = url.pathname.substring(url.pathname.indexOf("/path")+5)
@@ -12,17 +11,3 @@ export default async function proxy(request) {
     delete proxyRequest.headers.origin
     return await fetch(proxyRequest)
 }
-=======
-	const url = new URL(request.url);
-	const domain = atob(url.pathname.split("/")[2]);
-	const path = url.pathname.substring(url.pathname.indexOf("/path") + 5);
-	let proxyRequest = new Request(domain + path + url.search, {
-		body: request.body,
-		method: request.method,
-		headers: request.headers,
-	});
-	delete proxyRequest.headers.referer;
-	delete proxyRequest.headers.origin;
-	return await fetch(proxyRequest);
-}
->>>>>>> Line-Deno-Client/dev
