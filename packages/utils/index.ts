7e5375d9ab7360f7d9633168333cdb70ea322f6b
EdamAme-x <121654029+EdamAme-x@users.noreply.github.com>
2024-08-19 19:49:14 +0900

    add

---
+++ packages/utils/index.ts
@@ -0,0 +1,6 @@
+/**
+ * @module
+ * Utility for LINE
+ */
+
+export { LINE_SCHEME_URI } from './line-scheme/index.ts';
---
+++ packages/utils/line-scheme/index.ts
@@ -0,0 +1,11 @@
+/**
+ * @module
+ * Utility for LINE Scheme URI
+ */
+
+export const LINE_SCHEME_URI = {
+    /**
+     * Home URI
+     */
+    HOME: "line://home",
+} as const;
