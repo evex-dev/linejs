28b06aec4d6848e197c8b91fab55f4e9b7ddff5d
EdamAme-x <121654029+EdamAme-x@users.noreply.github.com>
2024-08-19 20:24:24 +0900

    Co-authored-by: skikozou <skikozou@users.noreply.github.com> Co-authored-by: piloking <piloking@users.noreply.github.com>

--- packages/utils/line-scheme/index.ts
+++ packages/utils/line-scheme/index.ts
@@ -3,9 +3,29 @@
  * Utility for LINE Scheme URI
  */
 
-export const LINE_SCHEME = {
-    /**
-     * Home URI
-     */
-    HOME: "line://home",
-} as const;
+type LINE_SCHEME_PREFIX =
+    | "line://"
+    | "http://line.me/R/"
+    | "https://line.me/R/"
+    | "http://line.naver.jp/R/"
+    | "https://line.naver.jp/R/"
+    // deno-lint-ignore ban-types
+    | (string & {});
+
+class LINE_SCHEME_BASE {
+    constructor(
+        public prefix: LINE_SCHEME_PREFIX = "line://",
+    ) {}
+
+    public getHome() {
+        return this.prefix + "home";
+    }
+
+    public getProfile() {
+        return this.prefix + "profile";
+    }
+}
+
+console.log(new LINE_SCHEME_BASE("line://").getHome())
+
+export { LINE_SCHEME_BASE as LINE_SCHEME };
