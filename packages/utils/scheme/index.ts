03a11569b92aaa586ffb4a12b735eec633a2fe57
test0987654321234567890 <test0987654321234567890@line-selfbot.deno.dev>
2024-08-20 04:46:08 +0900

    add line schemes

--- packages/utils/scheme/index.ts
+++ packages/utils/scheme/index.ts
@@ -188,7 +188,7 @@ class LINE_SCHEME_BASE {
 		return this.prefix + "nv/settings/themeSettings";
 	}
 
-	public getPrivacySettings(useSettings: boolean = false): string {
+	public getPrivacySettings(useSettings: boolean = true): string {
 		if (useSettings) {
 			return this.prefix + "nv/settings/privacy";
 		} else {
@@ -196,7 +196,7 @@ class LINE_SCHEME_BASE {
 		}
 	}
 
-	public getAccountSettings(useSettings: boolean = false): string {
+	public getAccountSettings(useSettings: boolean = true): string {
 		if (useSettings) {
 			return this.prefix + "nv/settings/account";
 		} else {
@@ -204,7 +204,7 @@ class LINE_SCHEME_BASE {
 		}
 	}
 
-	public getStickerSettings(useSettings: boolean = false): string {
+	public getStickerSettings(useSettings: boolean = true): string {
 		if (useSettings) {
 			return this.prefix + "nv/settings/sticker";
 		} else {
@@ -212,7 +212,7 @@ class LINE_SCHEME_BASE {
 		}
 	}
 
-	public getChatSettings(useSettings: boolean = false): string {
+	public getChatSettings(useSettings: boolean = true): string {
 		if (useSettings) {
 			return this.prefix + "nv/settings/chatSettings";
 		} else {
@@ -220,7 +220,7 @@ class LINE_SCHEME_BASE {
 		}
 	}
 
-	public getAddressBookSync(useSettings: boolean = false): string {
+	public getAddressBookSync(useSettings: boolean = true): string {
 		if (useSettings) {
 			return this.prefix + "nv/settings/addressBookSync";
 		} else {
@@ -228,7 +228,7 @@ class LINE_SCHEME_BASE {
 		}
 	}
 
-	public getTimelineSettings(useSettings: boolean = false): string {
+	public getTimelineSettings(useSettings: boolean = true): string {
 		if (useSettings) {
 			return this.prefix + "nv/settings/timelineSettings";
 		} else {
@@ -256,8 +256,177 @@ class LINE_SCHEME_BASE {
 		return this.prefix + `nv/chatMsg?chatId=${groupId}&messageId=${messageId}`;
 	}
 
-	public getRecommendOA(oaId:string): string {
+	public getRecommendOA(oaId: string): string {
 		return this.prefix + `nv/recommendOA/${oaId}`;
+	}
+
+	public getCh(ch: string = ""): string {
+		return this.prefix + "ch" + ch;
+	}
+
+	public getLiffApp(id: string = "", useLiff: boolean = false, param: Record<string, string> = {}): string {
+		const urlParam = "?" + new URLSearchParams(param).toString()
+		if (useLiff) {
+			return "https://" + "liff.line.me/" + id + urlParam;
+		} else {
+			return this.prefix + "app/" + id + urlParam;
+		}
+	}
+
+	public getOfficialAccountLiff(useLiff: boolean = false, param: Record<string, string> = {}): string {
+		return this.getLiffApp("1645278921-kWRPP32q", useLiff, param)
+	}
+
+	public getVoteLiff(useLiff: boolean = false, param: Record<string, string> = {}): string {
+		return this.getLiffApp("1477715170-Pl2JnXpR", useLiff, param)
+	}
+
+	public getSpamFilterLiff(useLiff: boolean = false, param: Record<string, string> = {}): string {
+		return this.getLiffApp("1556150347-zL2b31Eq", useLiff, param)
+	}
+
+	public getAutoReplyLiff(useLiff: boolean = false, param: Record<string, string> = {}): string {
+		return this.getLiffApp("1655702173-mvkgA1yR", useLiff, param)
+	}
+
+	public getTlanslateLiff(useLiff: boolean = false, param: Record<string, string> = {}): string {
+		return this.getLiffApp("1506931274-R5LDWmAW", useLiff, param)
+	}
+
+	public getGiftLiff(useLiff: boolean = false, param: Record<string, string> = {}): string {
+		return this.getLiffApp("1654120723-lYaWZEb6", useLiff, param)
+	}
+
+	public getSquareLiff(useLiff: boolean = false, param: Record<string, string> = {}): string {
+		return this.getLiffApp("1573545970-LlNdaE20", useLiff, param)
+	}
+
+	public getPointClubLiff(useLiff: boolean = false, param: Record<string, string> = {}): string {
+		return this.getLiffApp("1370466387-VxxzrzRW", useLiff, param)
+	}
+
+	public getScheduleLiff(useLiff: boolean = false, param: Record<string, string> = {}): string {
+		return this.getLiffApp("1655112642-8v0aXBwM", useLiff, param)
+	}
+
+	public getCouponLiff(useLiff: boolean = false, param: Record<string, string> = {}): string {
+		return this.getLiffApp("1611905212-3bydBEmv", useLiff, param)
+	}
+
+	public getStampShopLiff(useLiff: boolean = false, param: Record<string, string> = {}): string {
+		return this.getLiffApp("1359301715-JKd7Y7j1", useLiff, param)
+	}
+
+	public getDressUpShopLiff(useLiff: boolean = false, param: Record<string, string> = {}): string {
+		return this.getLiffApp("1359301715-lw9jxjqV", useLiff, param)
+	}
+
+	public getStampCategory(categoryId: string): string {
+		return this.prefix + `shop/sticker/category/${categoryId}`
+	}
+
+	public getStampPackage(packageId: string): string {
+		return this.prefix + `shop/sticker/detail/${packageId}`
+	}
+
+	public getStampAuthor(authorId: string): string {
+		return this.prefix + `shop/sticker/author/${authorId}`
+	}
+
+	public getStampProduct(productId: string): string {
+		return this.prefix + `shop/theme/detail?id=${productId}`
+	}
+
+	public getSquareMain(): string {
+		return this.prefix + "square/main"
+	}
+
+	public getSquareCreate(): string {
+		return this.prefix + "square/createSquare"
+	}
+
+	public getSquareReport(ticketOrEmid: string, isTicket: boolean = true): string {
+		if (isTicket) {
+			return this.prefix + `square/report?ticket=${ticketOrEmid}`
+		} else {
+			return this.prefix + `square/report?emid=${ticketOrEmid}`
+		}
+	}
+
+	public getSquareJoin(ticketOrEmid: string, isTicket: boolean = true): string {
+		if (isTicket) {
+			return this.prefix + `square/join?ticket=${ticketOrEmid}`
+		} else {
+			return this.prefix + `square/join?emid=${ticketOrEmid}`
+		}
+	}
+
+	public getSquareHome(emid: string): string {
+		return this.prefix + `square/home?encryptedSquareMid=${emid}`
+	}
+
+	public getSquareNotePost(squareMid: string, postId: string): string {
+		return this.prefix + `square/post?squareMid=${squareMid}&postId=${postId}&sourceType=TALKROOM_HOME`
+	}
+
+	public getUserTicket(ticket: string): string {
+		return this.prefix + `ti/p/${ticket}`
+	}
+
+	public getUserIdSearch(userId: string): string {
+		return this.prefix + `ti/p/~${userId}`
+	}
+
+	public getGroupTicket(ticket: string): string {
+		return this.prefix + `ti/g/${ticket}`
+	}
+
+	public getSquareTicket(ticket: string): string {
+		return this.prefix + `ti/g2/${ticket}`
+	}
+
+	public getTextShare(text: string, useShare: boolean = true): string {
+		if (useShare) {
+			return this.prefix + "share?text=" + text
+		} else {
+			return this.prefix + "msg/text/" + text
+		}
+	}
+
+	public getOaMessage(oaId: string, text: string): string {
+		return this.prefix + `oaMessage/${oaId}/?${text}`
+	}
+
+	public getAlbums(): string {
+		return this.prefix + "moa/albums/album"
+	}
+
+	public getUserPost(userMid: string, postId: string): string {
+		return this.prefix + `home/post?userMid=${userMid}&postId=${postId}`
+	}
+
+	public getOaMain(oaId_withoutAT: string): string {
+		return this.prefix + `home/public/main?id=${oaId_withoutAT}`
+	}
+
+	public getOaProfile(oaId_withoutAT: string): string {
+		return this.prefix + `home/public/profile?id=${oaId_withoutAT}`
+	}
+
+	public getOaPost(oaId_withoutAT: string, postId: string): string {
+		return this.prefix + `home/public/post?id=${oaId_withoutAT}&postId=${postId}`
+	}
+
+	public getGroupCreate(): string {
+		return this.prefix + "group/create"
+	}
+
+	public getGroupPost(groupId:string,postId:string): string {
+		return this.prefix + `group/home/posts/post?homeId=${groupId}&postId=${postId}`
+	}
+
+	public getGroupAlbum(albumId:string,groupId:string): string {
+		return this.prefix + `group/home/albums/album?albumId=${albumId}&homeId=${groupId}&albumIdV2=${albumId}`
 	}
 }
 
