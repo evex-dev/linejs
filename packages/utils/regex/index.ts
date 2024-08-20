b67a1fe5567f1a765b792fa1d792eae758894864
EdamAme-x <121654029+EdamAme-x@users.noreply.github.com>
2024-08-20 17:51:41 +0900

    update

--- packages/utils/regex/index.ts
+++ packages/utils/regex/index.ts
@@ -7,53 +7,53 @@
  * @description LINE Regex Utility
  */
 class LINE_REGEX_BASE {
-    constructor() {}
+	constructor() {}
 
-    /**
-     * Extracts a ticket from a given link using a regular expression.
-     *
-     * @param {string} link - The link to extract the ticket from.
-     * @return {string|null} The extracted ticket, or null if no match is found.
-     */
-    public getTicket(link: string): string | null {
-        const regex = /([a-zA-Z0-9-_]{10,38})[\?.+]?/;
+	/**
+	 * Extracts a ticket from a given link using a regular expression.
+	 *
+	 * @param {string} link - The link to extract the ticket from.
+	 * @return {string|null} The extracted ticket, or null if no match is found.
+	 */
+	public getTicket(link: string): string | null {
+		const regex = /([a-zA-Z0-9-_]{10,38})[\?.+]?/;
 
-        const matchResult = link.match(regex);
-        if (!matchResult) {
-            return null;
-        }
+		const matchResult = link.match(regex);
+		if (!matchResult) {
+			return null;
+		}
 
-        const ticket = matchResult.pop();
+		const ticket = matchResult.pop();
 
-        if (!ticket) {
-            return null;
-        }
+		if (!ticket) {
+			return null;
+		}
 
-        return ticket;
-    }
+		return ticket;
+	}
 
-    /**
-     * Extracts a emid from a given link using a regular expression.
-     *
-     * @param {string} link - The link to extract the ticket from.
-     * @return {string|null} The extracted ticket, or null if no match is found.
-     */
-    public getEmid(link: string): string | null {
-        const regex = /([a-zA-Z0-9-_]{59})[\?.+]?/;
+	/**
+	 * Extracts a emid from a given link using a regular expression.
+	 *
+	 * @param {string} link - The link to extract the ticket from.
+	 * @return {string|null} The extracted ticket, or null if no match is found.
+	 */
+	public getEmid(link: string): string | null {
+		const regex = /([a-zA-Z0-9-_]{59})[\?.+]?/;
 
-        const matchResult = link.match(regex);
-        if (!matchResult) {
-            return null;
-        }
+		const matchResult = link.match(regex);
+		if (!matchResult) {
+			return null;
+		}
 
-        const ticket = matchResult.pop();
+		const ticket = matchResult.pop();
 
-        if (!ticket) {
-            return null;
-        }
+		if (!ticket) {
+			return null;
+		}
 
-        return ticket;
-    }
+		return ticket;
+	}
 }
 
 export { LINE_REGEX_BASE as LINE_REGEX };
