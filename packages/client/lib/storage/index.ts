f1714acc3618ac8284f53ce5619cb7ee8449ca05
test0987654321234567890 <test0987654321234567890@line-selfbot.deno.dev>
2024-08-22 21:29:41 +0900

    -

--- packages/client/lib/storage/index.ts
+++ packages/client/lib/storage/index.ts
@@ -12,35 +12,36 @@ export interface Storage {
  * @description Base Storage for LINE Client
  */
 export abstract class BaseStorage {
-    /**
-     * @description Set a value.
-     * 
-     * @param {Storage["Key"]} key
-     * @param {Storage["Value"]} value
-     */
+	/**
+	 * @description Set a value.
+	 *
+	 * @param {Storage["Key"]} key
+	 * @param {Storage["Value"]} value
+	 */
 	public abstract set(key: Storage["Key"], value: Storage["Key"]): void;
 
-    /**
-     * @description Get a value.
-     * 
-     * @param {Storage["Key"]} key
-     * 
-     * @returns {Storage["Value"] | undefined}
-     */
+	/**
+	 * @description Get a value.
+	 *
+	 * @param {Storage["Key"]} key
+	 *
+	 * @returns {Storage["Value"] | undefined}
+	 */
 	public abstract get(key: Storage["Key"]): Storage["Value"] | undefined;
 
-    /**
-     * @description Delete a value.
-     * 
-     * @param {Storage["Key"]} key
-     */
+	/**
+	 * @description Delete a value.
+	 *
+	 * @param {Storage["Key"]} key
+	 */
 	public abstract delete(key: Storage["Key"]): void;
 
-    /**
-     * @description Clear a data.
-     */
+	/**
+	 * @description Clear a data.
+	 */
 	public abstract clear(): void;
 }
-
+/*
 export { MemoryStorage } from "./memory-storage.ts";
 export { FileStorage } from "./file-storage.ts";
+*/
