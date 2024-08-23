4375c444a25153a3692dc7833c9ae13f8c7ee814
EdamAme-x <121654029+EdamAme-x@users.noreply.github.com>
2024-08-23 20:27:21 +0900

    chore

--- packages/client/lib/typed-event-emitter/index.ts
+++ packages/client/lib/typed-event-emitter/index.ts
@@ -6,7 +6,6 @@ export class TypedEventEmitter<
 	T extends RecordEvent,
 	E extends keyof T = keyof T,
 > {
-
 	public listeners: Map<E, T[E][]> = new Map();
 
 	public on<E2 extends E>(event: E2, listener: T[E2]): void {
