1ce08dba7d19d47cd28e1b44324a7c7cd976dda2
EdamAme-x <121654029+EdamAme-x@users.noreply.github.com>
2024-08-25 17:13:57 +0900

    fix emitter

--- packages/client/libs/typed-event-emitter/index.ts
+++ packages/client/libs/typed-event-emitter/index.ts
@@ -8,24 +8,37 @@ export class TypedEventEmitter<
 > {
 	public listeners: Map<E, T[E][]> = new Map();
 
-	public on<E2 extends E>(event: E2, listener: T[E2]): void {
+	public on<E2 extends E>(event: E2, ...listeners: T[E2][]): this {
 		if (!this.listeners.has(event)) {
 			this.listeners.set(event, []);
 		}
-		this.listeners.get(event)?.push(listener);
-	}
 
-	public off<E2 extends E>(event: E2, listener: T[E2]): void {
-		if (this.listeners.has(event)) {
-			this.listeners
-				.get(event)
-				?.splice(this.listeners.get(event)?.indexOf(listener) ?? 0, 1);
+		for (const listener of listeners) {
+			this.listeners.get(event)?.push(listener);
 		}
+
+		return this;
 	}
 
-	public emit<E2 extends E>(event: E2, ...args: Parameters<T[E2]>): void {
+	public off<E2 extends E>(event: E2, ...listeners: T[E2][]): this {
+		if (this.listeners.has(event)) {
+			for (const listener of listeners) {
+				if (!this.listeners.get(event)?.includes(listener)) continue;
+
+				this.listeners
+					.get(event)
+					?.splice(this.listeners.get(event)?.indexOf(listener) ?? 0, 1);
+			}
+		}
+
+		return this;
+	}
+
+	public emit<E2 extends E>(event: E2, ...args: Parameters<T[E2]>): this {
 		if (this.listeners.has(event)) {
 			this.listeners.get(event)?.forEach((listener) => listener(...args));
 		}
+
+		return this;
 	}
 }
