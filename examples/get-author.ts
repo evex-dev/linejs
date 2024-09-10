10509658d2ab7fbeacc684e344d1a79b4683ba36
EdamAme-x <121654029+EdamAme-x@users.noreply.github.com>
2024-09-10 20:36:57 +0900

    re

--- examples/get-author.ts
+++ examples/get-author.ts
@@ -2,6 +2,18 @@ import { Client } from "@evex/linejs";
 
 const client = new Client();
 
+client.on("square:message", async (message) => {
+	const text = message.content;
+
+	if (text === "!me") {
+		await message.reply([
+            `Name: ${message.author.displayName}`,
+            `IconImage: ${message.author.iconImage}`,
+            `Mid: ${message.author.mid}`,
+        ].join("\n"));
+	}
+});
+
 client.on("message", async (message) => {
 	const text = message.content;
 
